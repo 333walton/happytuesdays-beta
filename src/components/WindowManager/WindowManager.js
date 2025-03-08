cc8bb3e9c28aaa68ede79afa754edc72a1c95dae
charles.seiler <charlieseiler@gmail.com>
2025-03-08 05:40:29 -0500

    project files update

--- src/components/WindowManager/WindowManager.js
+++ src/components/WindowManager/WindowManager.js
@@ -1,33 +1,100 @@
 import React, { Component } from "react";
 import * as Applications from "../Applications";
 import { ProgramContext } from "../../contexts";
+import * as icons from "../../icons";
 
 class WindowManager extends Component {
   static contextType = ProgramContext;
-
+  
+  // Track whether we've already opened apps for special URLs
+  state = {
+    doomOpened: false,
+    burnOpened: false
+  };
+  
+  componentDidMount() {
+    this.checkSpecialURLs();
+  }
+  
+  componentDidUpdate(prevProps) {
+    // Only check if the URL has changed or we haven't opened the apps yet
+    if (prevProps !== this.props) {
+      this.checkSpecialURLs();
+    }
+  }
+  
+  checkSpecialURLs() {
+    const pathname = window.location.pathname;
+    
+    // Check for Doom URL
+    if (pathname === "/doom" && !this.state.doomOpened) {
+      const hasDoom = Object.values(this.context.activePrograms).some(
+        prog => prog.component === "Doom"
+      );
+      
+      if (!hasDoom && this.context.onOpen) {
+        this.context.onOpen({
+          component: "Doom",
+          title: "DOOM",
+          icon: icons.doom32,
+        });
+        
+        // Mark that we've opened Doom for this URL
+        this.setState({ doomOpened: true });
+      }
+    } else if (pathname !== "/doom") {
+      // Reset the flag when we navigate away
+      this.setState({ doomOpened: false });
+    }
+    
+    // Check for Burn URL
+    if (pathname === "/burn" && !this.state.burnOpened) {
+      const hasBurn = Object.values(this.context.activePrograms).some(
+        prog => prog.component === "Burn"
+      );
+      
+      if (!hasBurn && this.context.onOpen) {
+        this.context.onOpen({
+          component: "Burn",
+          title: "Burn",
+          icon: icons.burn32, // Assuming there's a burn icon
+        });
+        
+        // Mark that we've opened Burn for this URL
+        this.setState({ burnOpened: true });
+      }
+    } else if (pathname !== "/burn") {
+      // Reset the flag when we navigate away
+      this.setState({ burnOpened: false });
+    }
+  }
+  
   render() {
     return (
       <>
-        {Object.keys(this.context.activePrograms)
-          // .filter(prog => !prog.minimized)
-          .map(progId => {
-            const prog = this.context.activePrograms[progId]
-            const Application = Applications[prog.component];
-            return (
-              <Application
-                {...prog}
-                save={this.context.save}
-                key={progId}
-                onClose={this.context.onClose}
-                onOpen={this.context.onOpen}
-                onMinimize={this.context.onMinimize}
-                moveToTop={this.context.moveToTop}
-                isActive={prog.id === this.context.activeId}
-                program={prog}
-                zIndex={this.context.zIndexes.indexOf(progId) + 5}
-              />
-            );
-          })}
+        {/* Render all active programs */}
+        {Object.keys(this.context.activePrograms).map(progId => {
+          const prog = this.context.activePrograms[progId];
+          const Application = Applications[prog.component];
+          
+          // Skip if application doesn't exist
+          if (!Application) return null;
+          
+          return (
+            <Application
+              {...prog}
+              save={this.context.save}
+              key={progId}
+              onClose={this.context.onClose}
+              onOpen={this.context.onOpen}
+              onMinimize={this.context.onMinimize}
+              moveToTop={this.context.moveToTop}
+              isActive={prog.id === this.context.activeId}
+              program={prog}
+              zIndex={this.context.zIndexes.indexOf(progId) + 5}
+            />
+          );
+        })}
       </>
     );
   }
