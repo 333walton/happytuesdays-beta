29989458fc9f05545e02dc37f4bd5e8396da5068
charles.seiler <charlieseiler@gmail.com>
2025-04-02 17:53:58 -0400

    test3

--- src/components/ImageWindow/ImageWindow.js
+++ src/components/ImageWindow/ImageWindow.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { createPortal } from "react-dom";
 import Window from "../tools/Window";
 import { WindowProgram, WindowAlert } from "packard-belle";
 import { paint16 } from "../../icons";
@@ -7,27 +6,21 @@ import buildMenu from "../../helpers/menuBuilder";
 import cx from "classnames";
 import "./_styles.scss";
 
-class ImageWindow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: 293,
-      height: 208,
-      showAlert: false
-    };
-  }
+// Utility function to detect mobile devices
+const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-  componentDidMount() {
-    const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-    const width = isMobile ? 300 : 293;
-    const height = isMobile ? 200 : 208;
-    this.setState({ width, height });
-  }
+class ImageWindow extends Component {
+  state = {
+    width: isMobile ? 300 : 293,
+    height: isMobile ? 200 : 208,
+    showAlert: false
+  };
 
   handleImageLoad = (e) => {
     const { naturalWidth, naturalHeight } = e.target;
-    const maxWidth = 800;
-    const maxHeight = 600;
+
+    const maxWidth = isMobile ? 300 : 800;
+    const maxHeight = isMobile ? 250 : 600;
     const minWidth = 282;
     const minHeight = 200;
 
@@ -47,61 +40,19 @@ class ImageWindow extends Component {
   };
 
   showAboutAlert = () => {
-    console.log("✅ showAboutAlert triggered");
+    console.log("showAboutAlert triggered");
     this.setState({ showAlert: true });
   };
 
   closeAboutAlert = () => {
-    console.log("✅ closeAboutAlert triggered");
+    console.log("closeAboutAlert triggered");
     this.setState({ showAlert: false });
-  };
-
-  renderAlert = () => {
-    const { showAlert } = this.state;
-    const { disclaimer } = this.props.data || {};
-
-    if (!showAlert) return null;
-
-    return createPortal(
-      <WindowAlert
-        title="Doodler's Abstract"
-        icon={paint16}
-        onOK={this.closeAboutAlert}
-        onClose={this.closeAboutAlert}
-        className="Window--active"
-        style={{
-          zIndex: 99999,
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "90vw",
-          maxWidth: "400px",
-          backgroundColor: "#fff",
-          padding: "10px",
-          display: "block",
-          border: "2px solid #000",
-          pointerEvents: "all",
-        }}
-      >
-        {disclaimer ? (
-          disclaimer
-        ) : (
-          <div style={{ padding: "0px 10px", margin: "0" }}>
-            <p><b>Doodle Name:</b> Test Doodle</p>
-            <p><b>Doodler:</b> CS</p>
-            <p><b>Date Submitted:</b> 3/31/25</p>
-            <p><b>Doodle Statement:</b> This is the first doodle submitted to the gallery</p>
-          </div>
-        )}
-      </WindowAlert>,
-      document.body // append outside Window context
-    );
   };
 
   render() {
     const { props, state } = this;
-    const { src, title } = props.data || {};
+    const { src, title, disclaimer } = props.data || {};
+    const { showAlert } = state;
 
     return (
       <>
@@ -119,12 +70,7 @@ class ImageWindow extends Component {
           initialHeight={state.height}
           className={cx("ImageWindow", props.className)}
         >
-          <div style={{
-            height: "auto",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
+          <div style={{ height: "auto", display: "flex", justifyContent: "center", alignItems: "center" }}>
             {src ? (
               <img
                 src={src}
@@ -146,12 +92,43 @@ class ImageWindow extends Component {
           </div>
         </Window>
 
-        {this.renderAlert()}
+        {showAlert && (
+          <WindowAlert
+            title="Doodler's Abstract"
+            icon={paint16}
+            onOK={this.closeAboutAlert}
+            onClose={this.closeAboutAlert}
+            className="Window--active"
+            style={{
+              zIndex: 1000,
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: isMobile ? "90%" : "75%",
+              maxWidth: isMobile ? "300px" : "400px",
+              maxHeight: isMobile ? "250px" : "300px",
+              padding: "10px",
+              backgroundColor: "#fff",
+              borderRadius: "8px",
+              overflow: "hidden"
+            }}
+          >
+            {disclaimer ? (
+              disclaimer
+            ) : (
+              <div style={{ padding: "0px 10px", margin: "0" }}>
+                <p><b>Doodle Name:</b> Test Doodle</p>
+                <p><b>Doodler:</b> CS</p>
+                <p><b>Date Submitted:</b> 3/31/25</p>
+                <p><b>Doodle Statement:</b> This is the first doodle submitted to the gallery</p>
+              </div>
+            )}
+          </WindowAlert>
+        )}
       </>
     );
   }
 }
 
 export default ImageWindow;
-
-
