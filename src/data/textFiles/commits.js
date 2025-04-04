aabbeed8d973ffe1f73fb744907d7c290380b900
charles.seiler <charlieseiler@gmail.com>
2025-04-04 18:02:58 -0400

    change log

--- src/data/textFiles/commits.js
+++ src/data/textFiles/commits.js
@@ -1,235 +1,68 @@
 const commits = `
-hash: f55082c
-date: 2025-03-28
-message: revert to 22a585
+============================================================
+>>HYDRA98 CHANGELOG.TXT
+============================================================
 
-hash: 222a585
-date: 2025-03-28
-message: helmet added
+[2025-03-28]  f55082c  - Revert to commit 22a585  
+[2025-03-28]  222a585  - Helmet added  
+[2025-03-28]  0918d32  - Video player  
+[2025-03-27]  5102286  - Video player program  
+[2025-03-27]  2794dbd  - Start menu organize  
+[2025-03-27]  af77882  - Recycling icon small  
+[2025-03-26]  88d690d  - Doodle small  
+[2025-03-26]  27d4d0c  - Doodles  
+[2025-03-26]  9e9f18f  - Start menu small  
+[2025-03-26]  c235b1c  - Minesweeper  
+[2025-03-23]  be5e8cb  - Small  
+[2025-03-23]  ea64742  - Small icons  
+[2025-03-22]  c397f81  - Hourglass ASCII  
+[2025-03-22]  53cffcb  - Pipes  
+[2025-03-21]  3d52a23  - Recycling CSS  
+[2025-03-20]  be900bf  - Small trash  
+[2025-03-20]  95d778f  - Trash link  
+[2025-03-20]  b987d0b  - Demoscene and misc start  
+[2025-03-20]  2963823  - Trash  
+[2025-03-20]  aed5376  - Small  
+[2025-03-20]  e6fdccc  - Start menu  
+[2025-03-20]  15d87ca  - Icons  
+[2025-03-18]  62aeb8c  - Vid player  
+[2025-03-18]  99c5728  - Small  
+[2025-03-18]  ac9d4f3  - Small  
+[2025-03-18]  86e4463  - Small  
+[2025-03-18]  890a804  - Small  
+[2025-03-17]  8a61318  - Start menu edits  
+[2025-03-16]  ea89dd4  - Small  
+[2025-03-16]  a7173d6  - Small  
+[2025-03-16]  e1e67fc  - Small  
+[2025-03-16]  b3ea98f  - Small  
+[2025-03-16]  3f068c4  - DOOM controls popup  
+[2025-03-15]  688a739  - Start menu edits  
+[2025-03-14]  f251334  - Desktop icon swap  
+[2025-03-14]  82e1330  - Error fix  
+[2025-03-14]  783db51  - Start menu changes  
+[2025-03-14]  734d2aa  - DOOM window load size  
+[2025-03-14]  34cb332  - More BIOS changes in index  
+[2025-03-14]  bd242e0  - Dawd  
+[2025-03-13]  af0ce0e  - More BIOS  
+[2025-03-13]  ddd8566  - More BIOS  
+[2025-03-13]  64059a2  - BIOS  
+[2025-03-13]  2368fb1  - OG metadata  
+[2025-03-13]  03f5fc4  - More readme  
+[2025-03-13]  417e365  - More readme  
+[2025-03-13]  bf2ef8b  - Readme and CSS edits  
+[2025-03-13]  3db22b3  - Updated image URL and site content  
+[2025-03-13]  2b69809  - Added Google Tag Manager  
+[2025-03-12]  094e6e8  - Favicon2  
+[2025-03-12]  572cafd  - Favicon  
+[2025-03-12]  3cf3c04  - Updated meta tags for Twitter and Discord previews  
+[2025-03-12]  eec766b  - Saving my local changes before pulling updates  
+[2025-03-12]  70fb2ea  - Update WindowManager.js for open in URL  
+[2025-03-08]  cd504a6  - Readme  
+[2025-03-08]  1210b74  - CSS edits  
+[2025-03-08]  cc8bb3e  - Project files update  
+[2025-03-06]  c3131e0  - Initial commit  
 
-hash: 0918d32
-date: 2025-03-28
-message: video player
-
-hash: 5102286
-date: 2025-03-27
-message: video player prog
-
-hash: 2794dbd
-date: 2025-03-27
-message: start menu organize
-
-hash: af77882
-date: 2025-03-27
-message: recycling icon small
-
-hash: 88d690d
-date: 2025-03-26
-message: doodle small
-
-hash: 27d4d0c
-date: 2025-03-26
-message: doodles
-
-hash: 9e9f18f
-date: 2025-03-26
-message: start menu small
-
-hash: c235b1c
-date: 2025-03-26
-message: minesweeper
-
-hash: be5e8cb
-date: 2025-03-23
-message: small
-
-hash: ea64742
-date: 2025-03-23
-message: small icons
-
-hash: c397f81
-date: 2025-03-22
-message: hourglass ascii
-
-hash: 53cffcb
-date: 2025-03-22
-message: pipes
-
-hash: 3d52a23
-date: 2025-03-21
-message: recycling css
-
-hash: be900bf
-date: 2025-03-20
-message: small trash
-
-hash: 95d778f
-date: 2025-03-20
-message: trash link
-
-hash: b987d0b
-date: 2025-03-20
-message: demoscene and misc start
-
-hash: 2963823
-date: 2025-03-20
-message: trash
-
-hash: aed5376
-date: 2025-03-20
-message: small
-
-hash: e6fdccc
-date: 2025-03-20
-message: start menu
-
-hash: 15d87ca
-date: 2025-03-20
-message: icons
-
-hash: 62aeb8c
-date: 2025-03-18
-message: vid player
-
-hash: 99c5728
-date: 2025-03-18
-message: small
-
-hash: ac9d4f3
-date: 2025-03-18
-message: small
-
-hash: 86e4463
-date: 2025-03-18
-message: small
-
-hash: 890a804
-date: 2025-03-18
-message: small
-
-hash: 8a61318
-date: 2025-03-17
-message: start menu edits
-
-hash: ea89dd4
-date: 2025-03-16
-message: small
-
-hash: a7173d6
-date: 2025-03-16
-message: small
-
-hash: e1e67fc
-date: 2025-03-16
-message: small
-
-hash: b3ea98f
-date: 2025-03-16
-message: small
-
-hash: 3f068c4
-date: 2025-03-16
-message: doom controls popup
-
-hash: 688a739
-date: 2025-03-15
-message: start menu edits
-
-hash: f251334
-date: 2025-03-14
-message: dt icon swap
-
-hash: 82e1330
-date: 2025-03-14
-message: error fix
-
-hash: 783db51
-date: 2025-03-14
-message: start menu changes
-
-hash: 734d2aa
-date: 2025-03-14
-message: doom window load size
-
-hash: 34cb332
-date: 2025-03-14
-message: more bios changes in index
-
-hash: af0ce0e
-date: 2025-03-13
-message: more bios
-
-hash: bd242e0
-date: 2025-03-14
-message: dawd
-
-hash: ddd8566
-date: 2025-03-13
-message: more bios
-
-hash: 64059a2
-date: 2025-03-13
-message: bios
-
-hash: 2368fb1
-date: 2025-03-13
-message: og metadata
-
-hash: 03f5fc4
-date: 2025-03-13
-message: more readme
-
-hash: 417e365
-date: 2025-03-13
-message: more readme
-
-hash: bf2ef8b
-date: 2025-03-13
-message: Readme and css edits
-
-hash: 3db22b3
-date: 2025-03-13
-message: Updated image URL and site content
-
-hash: 2b69809
-date: 2025-03-13
-message: Added Google Tag Manager
-
-hash: 094e6e8
-date: 2025-03-12
-message: favicon2
-
-hash: 572cafd
-date: 2025-03-12
-message: favicon
-
-hash: 3cf3c04
-date: 2025-03-12
-message: Updated meta tags for Twitter and Discord previews
-
-hash: eec766b
-date: 2025-03-12
-message: Saving my local changes before pulling updates
-
-hash: 70fb2ea
-date: 2025-03-12
-message: Update WindowManager.js for open in URL
-
-hash: cd504a6
-date: 2025-03-08
-message: readme
-
-hash: 1210b74
-date: 2025-03-08
-message: css editsg
-
-hash: cc8bb3e
-date: 2025-03-08
-message: project files update
-
-hash: c3131e0
-date: 2025-03-06
-message: Initial commit
+============================================================
 `;
 
 export default commits;
