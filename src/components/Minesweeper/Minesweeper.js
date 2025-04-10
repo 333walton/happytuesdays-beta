3ac602054b621d2e26946a1b4c36f83a27cd1bdf
charles.seiler <charlieseiler@gmail.com>
2025-04-09 21:13:01 -0400

    small minesweeper

--- src/components/Minesweeper/Minesweeper.js
+++ src/components/Minesweeper/Minesweeper.js
@@ -22,17 +22,21 @@ class Doom extends Component {
           },
         })}
         Component={WindowProgram}
-        initialHeight={240}
-        initialWidth={162}
+        initialHeight={247}
+        initialWidth={166}
         resizable={false}           // This disables resizing
         onMaximize={null}           // This disables the maximize button
-        className={cx("Doom", props.className)}
+        className={cx("Minesweeper", props.className)}
       >
         <iframe
           src="/dist/minesweeper222.html"
           title="Minesweeper"
           width="100%"
           height="100%"
+          style={{
+            border: "none",
+            left: "-1px"
+          }}
           frameBorder="0"
           scrolling="no"
         />
