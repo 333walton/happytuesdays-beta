ad537d9eb3895df08bfc2b0d2dcc451be2cd6aa8
charles.seiler <charlieseiler@gmail.com>
2025-04-29 10:15:06 -0400

    small

--- src/components/VideoPlayerwebkit/VideoPlayerwebkit.js
+++ src/components/VideoPlayerwebkit/VideoPlayerwebkit.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { WindowProgram } from "packard-belle";
 import cx from "classnames";
 import Window from "../tools/Window";
@@ -13,43 +13,39 @@ class VideoPlayer extends Component {
     const { props } = this;
     const videoSrc = props.data?.src || "/static/donwest.mp4";
     const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    const isMobile = /Mobi|Android/.test(navigator.userAgent) || isIOS;
-
-    const mobileHeight = isIOS ? '100%' : window.innerHeight * 0.35;
-    const mobileWidth = isIOS ? '100%' : window.innerWidth * 0.855;
-
+    
+    // Determine if we need iOS class
+    const classNames = cx("VideoPlayer", props.className, {
+      "ios-player": isIOS
+    });
+    
     return (
       <Window
         {...props}
         title="Video Player"
         icon={camera16}
         Component={WindowProgram}
-        initialHeight={isMobile ? mobileHeight : 290}
-        initialWidth={isMobile ? mobileWidth : 320}
-        resizable={!isMobile}
-        className={cx("VideoPlayer", props.className, {
-          "mobile-player": isMobile,
-          "ios-player": isIOS
-        })}
+        initialHeight={298}
+        initialWidth={322}
+        resizable={false} // Lock dimensions for consistency
+        className={classNames}
+        style={{ width: "322px", height: "298px" }} // Force dimensions
       >
-        <Video
-          src={videoSrc}
-          style={{
-            marginBottom: isMobile ? 0 : 4,
-            height: "100%",
-            width: "100%",
-            objectFit: "contain",
-            background: "#000",
-            maxWidth: "100vw",
-            maxHeight: isIOS ? "100vh" : "auto"
-          }}
-          controls={isMobile}
-          playsInline
-          webkit-playsinline="true"
-          preload="metadata"
-          controlsList="nodownload"
-          onContextMenu={(e) => e.preventDefault()}
-        />
+        <div className="video-content-wrapper">
+          <Video
+            src={videoSrc}
+            style={{
+              background: "#000",
+              width: "322px",
+              height: "298px"
+            }}
+            controls={false}
+            playsInline
+            preload="metadata"
+            controlsList="nodownload"
+            onContextMenu={(e) => e.preventDefault()}
+          />
+        </div>
       </Window>
     );
   }
